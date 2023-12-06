29e53cee2f2ac26f73e11457d55a2039f51d6268
Kavindu <kavindu@MacBook-Prox.local>
2023-12-06 14:14:22 +0530

    add iden3 merkle tree

---
+++ streaming/splitfile.go
@@ -0,0 +1,76 @@
+package streaming
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+)
+
+// SplitFile splits a file into chunks of a specified size
+func SplitFile(inputFile *os.File, chunkSize int64) ([]int64, error) {
+
+	// // Open the input file
+	// file, err := os.Open(inputFile)
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// Close the file, when the function exits
+	defer inputFile.Close()
+
+	// Get file info
+	fileInfo, err := inputFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	fileSize := fileInfo.Size()
+
+	// chunkNames := make([]string, 0)
+	chunkSizes := make([]int64, 0)
+	// hashValues := make([]string, 0)
+
+	hasher := sha256.New()
+
+	for i := int64(0); i < fileSize; i += chunkSize {
+		chunkName := fmt.Sprintf("%v_chunk%d", inputFile, i/chunkSize+1)
+
+		chunkFile, err := os.Create(chunkName)
+		if err != nil {
+			return nil, err
+		}
+
+		// Create a multi-writer to both write to the file and calculate the hash
+		multiWriter := io.MultiWriter(chunkFile, hasher)
+
+		// Copy the chunkSize bytes from the original file to the chunk file
+		_, err = io.CopyN(multiWriter, inputFile, chunkSize)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+
+		chunkFile.Close()
+		// chunkNames = append(chunkNames, chunkName)
+		chunkSizes = append(chunkSizes, chunkSize)
+
+		// Add the hash value of the chunk to the slice
+		hashValue := fmt.Sprintf("%x", hasher.Sum(nil))
+
+		// hashValues = append(hashValues, hashValue)
+		newChunkName := hashValue
+		err = os.Rename(chunkName, newChunkName)
+		if err != nil {
+			return nil, err
+		}
+
+		// chunkNames = append(chunkNames, newChunkName)
+		// hashValues = append(hashValues, hashValue)
+		// Reset the hash for the next iteration
+		hasher.Reset()
+
+	}
+
+	return chunkSizes, nil
+}
