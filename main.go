a938289a1c62b9b74f4d0c65590e771ff3715971
sithum-ravishka <sithumravishka1@gmail.com>
2023-12-06 20:26:19 +0530

    add merkle tree and data retrieve with proof check

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
-	"iden3-test/streaming"
+	"io"
 	"math/big"
 	"os"
 
@@ -12,60 +13,128 @@ import (
 	"github.com/iden3/go-merkletree-sql/db/memory"
 )
 
-func main() {
+func SplitFile(inputFile *os.File, chunkSize int64) ([]string, []string, error) {
+	defer inputFile.Close()
 
+	fileInfo, err := inputFile.Stat()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fileSize := fileInfo.Size()
+	var chunkNames []string
+	var hashValues []string
+
+	hasher := sha256.New()
+
+	for i := int64(0); i < fileSize; i += chunkSize {
+		chunkFile, err := os.Create(fmt.Sprintf("%v_chunk%d", inputFile.Name(), i/chunkSize+1))
+		if err != nil {
+			return nil, nil, err
+		}
+
+		multiWriter := io.MultiWriter(chunkFile, hasher)
+
+		_, err = io.CopyN(multiWriter, inputFile, chunkSize)
+		if err != nil && err != io.EOF {
+			return nil, nil, err
+		}
+
+		chunkFile.Close()
+		chunkNames = append(chunkNames, chunkFile.Name())
+
+		hashValue := fmt.Sprintf("%x", hasher.Sum(nil))
+		hashValues = append(hashValues, hashValue)
+
+		hasher.Reset()
+	}
+
+	return chunkNames, hashValues, nil
+}
+
+func RetrieveChunksAndVerify(chunkNames []string, hashValues []string, outputFileName string) error {
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	hasher := sha256.New()
+
+	for i, chunkName := range chunkNames {
+		chunkFile, err := os.Open(chunkName)
+		if err != nil {
+			return err
+		}
+		defer chunkFile.Close()
+
+		// Create a multi-reader to both read from the file and calculate the hash
+		multiReader := io.TeeReader(chunkFile, hasher)
+
+		_, err = io.Copy(outputFile, multiReader)
+		if err != nil {
+			return err
+		}
+
+		// Verify the hash of the chunk
+		hashValue := fmt.Sprintf("%x", hasher.Sum(nil))
+		if hashValue != hashValues[i] {
+			return fmt.Errorf("hash verification failed for chunk %s", chunkName)
+		}
+
+		// Reset the hash for the next iteration
+		hasher.Reset()
+	}
+
+	return nil
+}
+
+func main() {
+	chunkSize := int64(500000)
 	inputFile, err := os.Open("test.png")
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return
 	}
-	chunkSize := int64(256000) // Set your desired chunk size in bytes
+	defer inputFile.Close()
 
-	// Split the file into chunks
-	chunk, err := streaming.SplitFile(inputFile, chunkSize)
+	chunkNames, hashValues, err := SplitFile(inputFile, chunkSize)
 	if err != nil {
-		fmt.Println("Error splitting file:", err)
+		fmt.Println("Error splitting and hashing file:", err)
 		return
 	}
 
-	// Sparse MT
 	ctx := context.Background()
-
-	// Tree storage
 	store := memory.NewMemoryStorage()
-
-	// Generate a new MerkleTree with 32 levels
 	mt, _ := merkletree.NewMerkleTree(ctx, store, 32)
 
-	// Add a leaf to the tree with index 1 and chunk[0]
-	// index1 := big.NewInt(1)
-	// value1 := big.NewInt(chunk[0])
-	// mt.Add(ctx, index1, value1)
+	for index, value := range chunkNames {
+		mt.Add(ctx, big.NewInt(int64(index)), big.NewInt(0)) // You might need to adjust the second parameter based on your use case
+		fmt.Println(ctx, index, value)
 
-	// // Add another leaf to the tree
-	// index2 := big.NewInt(2)
-	// value2 := big.NewInt(chunk[1])
-	// mt.Add(ctx, index2, value2)
+		// Proof of membership for each chunk
+		proofExist, _, _ := mt.GenerateProof(ctx, big.NewInt(int64(index)), mt.Root())
+		fmt.Printf("Proof of membership for chunk %d: %v\n", index, proofExist.Existence)
 
-	for i, val := range chunk {
-		mt.Add(ctx, big.NewInt(int64(i)), big.NewInt(val))
+		err := newFunction(proofExist, chunkNames, hashValues, "restored_data.jpg")
+		if err != nil {
+			fmt.Println("Error retrieving and verifying chunks:", err)
+			return
+		}
 	}
 
-	// Proof of membership of a leaf with index 1
-	proofExist, value, _ := mt.GenerateProof(ctx, big.NewInt(0), mt.Root())
+	// Proof of non-membership for a non-existing chunk (e.g., index 100)
+	nonExistingIndex := big.NewInt(100)
+	proofNotExist, _, _ := mt.GenerateProof(ctx, nonExistingIndex, mt.Root())
+	fmt.Printf("Proof of non-membership for chunk %d: %v\n", nonExistingIndex.Int64(), proofNotExist.Existence)
 
-	fmt.Println("Proof of membership 1:", proofExist.Existence)
-	fmt.Println("Value corresponding to the queried index:", value)
-
-	// Proof of non-membership of a leaf with index 4
-	proofNotExist, _, _ := mt.GenerateProof(ctx, big.NewInt(6), mt.Root())
-
-	fmt.Println("Proof of membership 4:", proofNotExist.Existence)
-
-	fmt.Printf("%v\n", mt)
-
-	// transform root from bytes array to json
 	claimToMarshal, _ := json.Marshal(mt.Root())
-
 	fmt.Println(string(claimToMarshal))
+}
 
+func newFunction(proofExist *merkletree.Proof, chunkNames []string, hashValues []string, outputFileName string) error {
+	if proofExist.Existence {
+		return RetrieveChunksAndVerify(chunkNames, hashValues, outputFileName)
+	}
+	return fmt.Errorf("proof of non-membership received")
 }
