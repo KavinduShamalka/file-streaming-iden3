af1d82bc1667a98845db45e8f4899b79ded55846
Kavindu <kavindu@192.168.1.3>
2023-12-07 11:08:03 +0530

    Documenting the retrieve.go

--- streaming/retrieve.go
+++ streaming/retrieve.go
@@ -10,19 +10,29 @@ import (
 // Retrieve Chunks and Verify.
 func RetrieveChunksAndVerify(chunkNames []string, hashValues []string, outputFileName string) error {
 
+	// Create output file name
 	outputFile, err := os.Create(outputFileName)
+
+	// Check if there is any error
 	if err != nil {
 		return err
 	}
+
+	// Defer is used to ensure that a function call is performed later in a program’s execution, usually for purposes of cleanup.
+	// defer: Allows us to "clean up" resources even if the function encounters errors.
+	// close: Can return errors indicating problems with resource release, which can be handled appropriately.
 	defer outputFile.Close()
 
+	// Use sha256 as the hashing algorithum
 	hasher := sha256.New()
 
+	// get the index and the chunknames from "chunkNames" slice
 	for i, chunkName := range chunkNames {
 		chunkFile, err := os.Open(chunkName)
 		if err != nil {
 			return err
 		}
+
 		defer chunkFile.Close()
 
 		// Create a multi-reader to both read from the file and calculate the hash
